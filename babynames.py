1b2b80db2cda7b9d953487237528c0ccf1506a12
Davor Telisman <davortelisman@gmail.com>
2023-07-24 16:13:07 +0200

    Suggested milestones for incremental development:  -Extract the year and print it  -Extract the names and rank numbers and just print them  -Get the names data into a dict and print it  -Build the [year, 'name rank', ... ] list and print it  -Fix main() to use the extract_names list """
     Given a file name for baby.html, returns a list starting with the year string
      followed by the name-rank strings in alphabetical order.
      ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
      """

--- babynames.py
+++ babynames.py
@@ -1,28 +1,46 @@
-import sys
 import re
 
-names = []
 
 def extract_names(filename):
     with open(filename, 'r') as file:
-        html_content = file.read()
+        content = file.read()
 
-        # Use regular expressions to find the names in the HTML content
-        # This pattern assumes that the name is in the second column of a table.
-        pattern = r'<tr align="right"><td>\d+</td><td>(\w+)</td><td>\w+</td>'
-        matches = re.findall(pattern, html_content)
+    # Extract the year from the filename using regular expressions
+    year_match = re.search(r'baby(\d{4})\.html', filename)
+    if year_match:
+        year = year_match.group(1)
+    else:
+        print(f"Could not extract the year from {filename}")
+        return []
 
-        # Extend the 'names' list with the extracted names
-        names.extend(matches)
+    # Extract names and rank numbers using regular expressions
+    name_rank_pattern = re.findall(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', content)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python babynames.py <filename>")
-        sys.exit(1)
+    # Sort the name-rank strings in alphabetical order
+    name_rank_list = sorted(
+        [f"{boy_name} {rank}", f"{girl_name} {rank}"] for rank, boy_name, girl_name in name_rank_pattern)
 
-    filename = sys.argv[1]
-    extract_names(filename)
+    # Flatten the list and prepend the year
+    result_list = [year] + [item for sublist in name_rank_list for item in sublist]
 
-    # Print the extracted names
-    for i, name in enumerate(names, start=1):
-        print(f"Rank #{i}: {name}")
+    return result_list
+
+
+# List of HTML files
+html_files = [
+    "baby1990.html",
+    "baby1992.html",
+    "baby1994.html",
+    "baby1996.html",
+    "baby1998.html",
+    "baby2000.html",
+    "baby2002.html",
+    "baby2004.html",
+    "baby2006.html",
+    "baby2008.html"
+]
+
+# Build and print the [year, 'name rank', ... ] list for each HTML file
+for filename in html_files:
+    data_list = extract_names(filename)
+    print(data_list)
